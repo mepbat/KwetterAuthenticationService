0d5b7e3e5373693a6acc61412b87967de60de1d1
i383656 <s.westerburger@student.fontys.nl>
2021-03-15 10:02:04 +0100

    first commit

---
+++ src/main/java/fontys/ict/kwetter/KwetterAuthenticationService/KwetterAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package fontys.ict.kwetter.KwetterAuthenticationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwetterAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwetterAuthenticationServiceApplication.class, args);
+	}
+
+}
